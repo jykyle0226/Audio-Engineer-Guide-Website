0b558818e4c3358ab1f79cb7e0fb6390708a4e93
jykyle0226 <jykyle0226@gmail.com>
2022-08-10 16:31:56 -0400

    Spotify

--- src/pages/APIpage/Spotify.js
+++ src/pages/APIpage/Spotify.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "../../Style/spotify.css";
 function Spotify() {
   const CLIENT_ID = "c8a2306be41f4413ad5578215afa8c04";
-  const REDIRECT_URI = "http://iridescent-croquembouche-9e80f7.netlify.app/Spotify";
+  const REDIRECT_URI = "https://iridescent-croquembouche-9e80f7.netlify.app/Spotify";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
 
